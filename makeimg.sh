37f0c46cbceb3b39f64f796b669c4730b4043bc0
campital <33563097+campital@users.noreply.github.com>
2018-01-16 18:10:33 -0500

    VS Code test

--- makeimg.sh
+++ makeimg.sh
@@ -1,9 +1,13 @@
+# Builds the program. May specify -nofi to skip UEFI building
+
+if [ "$1" != "-nofi" ]; then
 cd UEFI/vUDK2017
 . edksetup.sh
 build -p DuetPkg/DuetPkgX64.dsc
 cd ..
 cp vUDK2017/Build/DuetPkgX64/DEBUG_GCC5/X64/BOOTSRC/boot_main/DEBUG/BOOTLOAD.efi BOOTX64.efi
 cd ..
+fi
 
 rm -rf img_files
 mkdir -p img_files/EFI/BOOT
